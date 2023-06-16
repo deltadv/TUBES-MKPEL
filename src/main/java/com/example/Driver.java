e20c6c59dece79222ab66aa4fcc8a847caa01b93
Deva Raja <125891830+deltadv@users.noreply.github.com>
2023-06-16 15:17:38 +0700

    Update Driver.java

--- src/main/java/com/example/Driver.java
+++ src/main/java/com/example/Driver.java
@@ -10,10 +10,13 @@ public class Driver
 		
 		// Memanggil metode hitungLuasPersegi() dari objek fungsi
         int luasPersegi = persegi.hitungLuasPersegi(sisi);
+	System.out.println("Hasil perhitungan");
         System.out.println("Luas persegi adalah: " + luasPersegi);
+	    
 
         // Memanggil metode hitungKelilingPersegi() dari objek fungsi
         int kelilingPersegi = persegi.hitungKelilingPersegi(sisi);
+	System.out.println("Hasil perhitungan");
         System.out.println("Keliling persegi adalah: " + kelilingPersegi);
     }
 }
